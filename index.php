e8cdc5ff90673a46282ce5a2fbfe7a3e3aaa337e
raveenavergis <32518011+raveenavergis@users.noreply.github.com>
2018-05-30 12:37:48 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
- ehco "Hello World";
- ?>
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
+?>
